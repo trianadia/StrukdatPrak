6cf750d2e5321cb4a3bb844d535ac0bba3b47828
Nadia Tria <240605110136@student.uin-malang.ac.id>
2025-11-20 03:56:41 +0000

    fghjk

---
+++ buku/ClassArray.java
@@ -0,0 +1,19 @@
+package buku;
+
+public class ClassArray {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
